4c720e16e0b1c6808ec3e5a9be474946faf71877
juanda321 <juandavidaguirre455@gmail.com>
2025-03-16 18:02:12 -0500

    comit

---
+++ frontend/src/config.js
@@ -0,0 +1,4 @@
+const API_URL = "https://f4e5-2800-484-a585-1a80-94ab-eb7e-ea0f-1c66.ngrok-free.app"; // 🔥 Asegúrate de actualizar esta URL cuando reinicies Ngrok
+export default API_URL;
+
+//pruebas

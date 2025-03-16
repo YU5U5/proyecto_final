37f51664d27d6f9d1b1ace9cc3f35520b1dbbade
juanda321 <juandavidaguirre455@gmail.com>
2025-03-16 18:10:39 -0500

    commit 3

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-const API_URL = "https://f4e5-2800-484-a585-1a80-94ab-eb7e-ea0f-1c66.ngrok-free.app"; // 🔥 Asegúrate de actualizar esta URL cuando reinicies Ngrok
+const API_URL = "https://f4e5-2800-484-a585-1a80-94ab-eb7e-ea0f-1c66.ngrok-free.a"; // 🔥 Asegúrate de actualizar esta URL cuando reinicies Ngrok
 export default API_URL;
 

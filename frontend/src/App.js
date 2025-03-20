44d64a0c5f5f0e4c4ee4c8611e4a1310875a71fe
juanda321 <juandavidaguirre455@gmail.com>
2025-03-20 09:27:54 -0500

    commit i

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,18 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./componentes/Login";
 import Register from "./componentes/Register";
 import RecoverPassword from "./componentes/RecoverPassword";
+//import Procedures from "./componentes/Procedures.jsx";
+
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />  // ✅ Página de inicio
+        
         <Route path="/login" element={<Login />} />  // 🔥 Se agrega la ruta correcta para login
         <Route path="/register" element={<Register />} />
         <Route path="/recover-password" element={<RecoverPassword />} />
+        {/* <Route path="/Procedures" element={<Procedures />} /> */}
       </Routes>
     </Router>
   );
